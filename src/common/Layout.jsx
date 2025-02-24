0382810ae6294704a0a0ae29f6f3632e54185a3d
Jesús García García <jesus.garcia@cojali.com>
2025-02-24 18:09:25 +0100

    Footer

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router";
 
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 export default function Layout() {
     return (<>
-    <Header></Header>
-    <main>
-        <Outlet/>
-    </main>
-    {/*<Footer></Footer>*/}
+        <Header/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
     </>);
 }
